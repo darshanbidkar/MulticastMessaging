13ffe6ac01a97dfb14f257db18699175afd98644
Darshan Bidkar <darshan.bidkar@gmail.com>
2015-07-28 02:20:00 -0500

    left out

---
+++ src/com/multicast/networking/NetworkInterface.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package com.multicast.networking;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+import com.multicast.handlers.TCPHandler;
+import com.multicast.handlers.UDPHandler;
+
+/**
+ * @author darshanbidkar
+ *
+ */
+public abstract class NetworkInterface {
+
+	private ServerSocket mServerSocket;
+	private DatagramSocket mUDPSocket;
+	private volatile boolean isRunning = true;
+	protected UDPHandler udphandler;
+
+	public NetworkInterface() {
+		createUDPServer();
+		createTCPServer();
+	}
+
+	public NetworkInterface(boolean isProxy) {
+		createUDPServer();
+		if (isProxy) {
+			createTCPServer();
+		}
+	}
+
+	private void createUDPServer() {
+		try {
+			mUDPSocket = new DatagramSocket(5051);
+			udphandler = new UDPHandler(mUDPSocket, this);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void createTCPServer() {
+		try {
+			mServerSocket = new ServerSocket(5050);
+			new Thread() {
+				@Override
+				public void run() {
+					while (isRunning) {
+						Socket sock;
+						try {
+							sock = mServerSocket.accept();
+
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void closeServers() {
+
+	}
+
+	protected TCPHandler createClientSocket(String parentIP) {
+		try {
+			Socket socket = new Socket(parentIP, 5050);
+			TCPHandler handler = new TCPHandler(socket, this);
+			return handler;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private void closeTCPServer() {
+		if (this.isRunning && this.mServerSocket != null) {
+			System.out.println("closing server");
+			this.isRunning = false;
+			try {
+				this.mServerSocket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public abstract void messageReceived(String message);
+
+	public abstract boolean shouldAccommodate();
+
+	public abstract void addConnection(Socket newSock);
+}
